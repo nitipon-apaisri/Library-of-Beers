0c5edcd1410770a4733a88b8def5f283bf066c65
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-11-05 17:58:16 +0100

    added a modal function when clicked See more button

--- js/main.js
+++ js/main.js
@@ -6,11 +6,20 @@ let fetchingBeers = async () => {
          allBeers.push(beers[0]);
       });
 };
+let more = document.querySelector(".card > .card-footer > button");
+let modal = document.querySelector(".modal");
+more.addEventListener("click", () => {
+   modal.classList.add("active");
+});
+let closeButt = document.querySelector(".modal > .modal-container > .modal-header > .close");
+closeButt.addEventListener("click", () => {
+   modal.classList.remove("active");
+});
 let rand = document.querySelector(".random");
 rand.addEventListener("click", async () => {
    allBeers = [];
    await fetchingBeers();
-   let name = document.querySelector(".card > .card-header > .card-title > h3");
+   let name = document.querySelector(".card > .card-header > .card-title > .name");
    name.innerHTML = allBeers[0].name;
    console.log(allBeers[0].name);
 });
