a1cc378e38ded00cf0c5704d90afce451fcfd345
nitipon apaisri <nitipon.apaisri@gmail.com>
2020-11-06 11:58:12 +0100

    able to find a beer by name

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ let rand = document.querySelector(".random");
 rand.addEventListener("click", async () => {
    randBeer = [];
    await fetchingBeers();
-   randomBeerInfo();
+   randomBeerInfo(0);
    console.log(randBeer[0].ingredients.malt);
    console.log(randBeer[0].ingredients.hops);
    console.log(randBeer[0].food_pairing);
@@ -42,19 +42,19 @@ let closeButt = document.querySelector(".modal > .modal-container > .modal-heade
 closeButt.addEventListener("click", () => {
    modal.classList.remove("active");
 });
-const randomBeerInfo = () => {
+const randomBeerInfo = (value) => {
    let name = document.querySelector(".card > .card-header > .card-title > .name");
    let img = document.querySelector(".card > .card-img > img ");
-   img.src = randBeer[0].image_url;
-   name.innerHTML = randBeer[0].name;
-   for (let beerIngredients of randBeer[0].ingredients.malt) {
+   img.src = randBeer[value].image_url;
+   name.innerHTML = randBeer[value].name;
+   for (let beerIngredients of randBeer[value].ingredients.malt) {
       let ingredientsList = document.createElement("li");
       let ingredientsUserList = document.querySelector(".modal-content > ul");
       let ingredients = beerIngredients.name;
       ingredientsList.textContent = ingredients;
       ingredientsUserList.appendChild(ingredientsList);
    }
-   for (let beerHops of randBeer[0].ingredients.hops) {
+   for (let beerHops of randBeer[value].ingredients.hops) {
       let hopsList = document.createElement("li");
       let hopsUserList = document.querySelector(".modal-content > .hops");
       let hops = beerHops.name;
@@ -62,7 +62,7 @@ const randomBeerInfo = () => {
       hopsUserList.appendChild(hopsList);
    }
 
-   for (let beerPairing of randBeer[0].food_pairing) {
+   for (let beerPairing of randBeer[value].food_pairing) {
       let foodList = document.createElement("li");
       let foodUserList = document.querySelector(".modal-content > .food-pairing");
       let food = beerPairing;
@@ -76,33 +76,59 @@ fetch("https://api.punkapi.com/v2/beers")
    .then((res) => res.json())
    .then((beers) => {
       for (let i of beers) {
-         allBeers.push(i.name);
+         allBeers.push(i);
       }
       console.log(allBeers);
    });
 let inputBeerName = document.querySelector(".searching-page > section > .empty > .empty-action > .input-beer-name");
 let submit = document.querySelector(".name-submit");
 submit.addEventListener("click", () => {
-   // const options = {
-   //    isCaseSensitive: false,
-   //    includeScore: false,
-   //    shouldSort: true,
-   //    includeMatches: false,
-   //    findAllMatches: false,
-   //    minMatchCharLength: 1,
-   //    location: 0,
-   //    threshold: 0.6,
-   //    distance: 100,
-   //    useExtendedSearch: false,
-   //    ignoreLocation: false,
-   //    ignoreFieldNorm: false,
-   //    keys: ["name"],
-   // };
+   let more1 = document.querySelector(".list > .card > .card-footer > button");
+   for (let i of allBeers) {
+      let v = i.name.includes(inputBeerName.value);
+      if (v == true) {
+         let name = document.querySelector(".searching-page > section.list >  .card > .card-header > .card-title > .name");
+         let img = document.querySelector(".searching-page > section.list >.card > .card-img > img ");
+         img.src = i.image_url;
+         name.innerHTML = i.name;
+         more1.addEventListener("click", () => {
+            console.log("hi");
+            modal.classList.add("active");
+            let nameInModal = document.querySelector(".modal-title");
+            nameInModal.innerHTML = i.name;
+            let modalImg = document.querySelector(".modal > .modal-container > .modal-body > .modal-content > .card-img > img");
+            modalImg.src = i.image_url;
+            let modaldescription = document.querySelector(".description");
+            modaldescription.innerHTML = `<b>Description:</b> ${i.description}`;
+            let modalAlco = document.querySelector(".alco-volume");
+            modalAlco.innerHTML = `<b>Volume:</b> ${i.volume.value} ${i.volume.unit}`;
+            let tips = document.querySelector(".tips");
+            tips.innerHTML = `<b>Tips:</b> ${i.brewers_tips}`;
+            for (let beerIngredients of i.ingredients.malt) {
+               let ingredientsList = document.createElement("li");
+               let ingredientsUserList = document.querySelector(".modal-content > ul");
+               let ingredients = beerIngredients.name;
+               ingredientsList.textContent = ingredients;
+               ingredientsUserList.appendChild(ingredientsList);
+            }
+            for (let beerHops of i.ingredients.hops) {
+               let hopsList = document.createElement("li");
+               let hopsUserList = document.querySelector(".modal-content > .hops");
+               let hops = beerHops.name;
+               hopsList.textContent = hops;
+               hopsUserList.appendChild(hopsList);
+            }
 
-   // const fuse = new Fuse(allBeers, options);
-   // const re = fuse.search(inputBeerName.value);
-   // console.log(re);
-   let v = allBeers.includes(inputBeerName.value);
-   console.log(v);
+            for (let beerPairing of i.food_pairing) {
+               let foodList = document.createElement("li");
+               let foodUserList = document.querySelector(".modal-content > .food-pairing");
+               let food = beerPairing;
+               foodList.textContent = food;
+               foodUserList.appendChild(foodList);
+            }
+         });
+         break;
+      }
+   }
    inputBeerName.value = "";
 });
